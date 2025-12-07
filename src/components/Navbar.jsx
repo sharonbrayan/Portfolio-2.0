64f1f82def685a41d97a9339b0270a86502e30b2
sharonbrayan <sharonbrayan087@gmail.com>
2025-12-07 20:13:12 +0530

    chore: minor update to navbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,8 @@ const navElements = [
 ]
 const Navbar = () => {
     return (
-        <nav className='fixed-top d-flex justify-content-between align-items-center border mt-2 p-3 rounded rounded-4 border-1 nav-bar'>
-            <div className='w-50'><span className='fs-3 fw-bold'>Sharon</span></div>
-            <div className='w-50 d-flex justify-content-around'>
+        <nav className='fixed-top d-flex justify-content-between align-items-center border mt-2 p-3 rounded rounded-4 border-2 nav-bar'>
+            <div className='d-flex justify-content-around w-100'>
                 {navElements.map((item) => (
                     <a key={item.ele} href={item.link} className='text-decoration-none text-black'>{item.ele}</a>
                 ))}
